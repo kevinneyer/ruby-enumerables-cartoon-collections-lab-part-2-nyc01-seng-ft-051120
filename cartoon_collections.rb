8c89da47cddae071d8dc4399357524222cedc5e4
Kevin Neyer <kevinneyer@gmail.com>
2020-04-14 19:01:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -34,7 +34,7 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   
   planeteer_calls.detect do |n|
-    n == valid_calls.find  
+    n == valid_calls.each
   end 
 end 
 
